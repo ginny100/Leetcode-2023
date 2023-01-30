0b11cf535c525ac42f0ac70add0d42ec56d8647f
Ginny-Dang <ginnydang0728@gmail.com>
2023-01-29 19:50:35 -0600

    01/29/2023

--- 1. January/Day29LFUCache.cpp
+++ 1. January/Day29LFUCache.cpp
@@ -11,15 +11,15 @@
 
 class LFUCache
 {
-    unordered_map<int, pair<int, list<pair<int, int>>::iterator>> cache;
-    unordered_map<int, list<pair<int, int>>> frequencies;
-    int minFreq;  // the minimum frequency at any given time
-    int capacity; // the capacity given in the input
+    unordered_map<int, pair<int, list<pair<int, int>>::iterator>> cache; // key : (freq : iterator pointing to a list of {key : value} pairs) in the other map
+    unordered_map<int, list<pair<int, int>>> frequencies;                // freq : {{key1 : value1}, {key2 : value2}, {key3 : value3}, ...}
+    int minFreq;                                                         // the minimum frequency at any given time
+    int capacity;                                                        // the capacity given in the input
 
     void insert(int key, int val, int freq)
     {
         frequencies[freq].push_back({key, val});
-        cache[key] = {freq, --frequencies[freq].end()};
+        cache[key] = {freq, --frequencies[freq].end()}; // --frequencies[freq].end() refers to the very last element of the list frequencies[freq]
     }
 
 public:
@@ -28,18 +28,20 @@ public:
 
     int get(int key)
     {
-        const auto keyIt = cache.find(key);
+        // Get the iterator pointing to an entry in cache
+        const auto keyItInCache = cache.find(key);
 
         // If this key does not exist in the cache
-        if (keyIt == cache.end())
+        if (keyItInCache == cache.end())
             return -1;
 
         // If this key exists in the cache
-        const int freq = keyIt->second.first;
-        const auto freqIt = keyIt->second.second;
-        const pair<int, int> kv = *freqIt;
+        const pair<int, pair<int, list<pair<int, int>>::iterator>> keyEntryInCache = *keyItInCache;
+        const int freq = keyEntryInCache.second.first;     // The frequency of this key
+        const auto freqIt = keyEntryInCache.second.second; // The iterator associating with the frequency of this key
+        const pair<int, int> kv = *freqIt;                 // The actual (key : value) pair associating with freq in the frequencies unordered map
         //// Update frequencies and the min frequency
-        frequencies[freq].erase(freqIt);
+        frequencies[freq].erase(freqIt); // Can be understood as removing the old frequency of this key
         if (frequencies[freq].empty() && minFreq == freq)
             minFreq++;
         //// Insert this key to the cache
@@ -52,13 +54,15 @@ public:
         if (capacity <= 0)
             return;
 
-        const auto keyIt = cache.find(key);
+        // Get the iterator pointing to an entry in cache
+        const auto keyItInCache = cache.find(key);
 
         // If this key is already in the cache
-        if (keyIt != cache.end())
+        if (keyItInCache != cache.end())
         {
-            // Update the value and frequency of this key
-            keyIt->second.second->second = value;
+            // Update the (new) value of this key
+            keyItInCache->second.second->second = value; // keyItInCache->second is the frequency of this key
+            // Update the new frequency of this key
             get(key);
             return;
         }
@@ -66,8 +70,10 @@ public:
         //// If the capacity has been full
         if (cache.size() == capacity)
         {
+            // Get the least frequently used key
+            int leastFreqUsedKey = frequencies[minFreq].front().first;
             // Erase the least frequently used key
-            cache.erase(frequencies[minFreq].front().first);
+            cache.erase(leastFreqUsedKey);
             frequencies[minFreq].pop_front();
         }
         //// Update the cache with the new key
