5469c98504c43607d3274cd0bd7f0e51c49614c5
Ginny-Dang <ginnydang0728@gmail.com>
2023-04-26 19:13:23 -0500

    04/27/2023

---
+++ 4. April/Day27BulbSwitcher.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file Day27BulbSwitcher.cpp
+ * @author Ginny Dang
+ * @brief 319. Bulb Switcher (https://leetcode.com/problems/bulb-switcher/description/)
+ * @version 0.1
+ * @date 2023-04-26
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+class Solution
+{
+public:
+    int bulbSwitch(int n)
+    {
+        return sqrt(n);
+    }
+};
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
